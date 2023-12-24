4ab2e078b3b9392e517ece4bcca09eef851c5448
Vasyl <vasapanov721@gmail.com>
2023-12-24 10:06:56 +0100

    Fixed back button in error page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import FormBtn from 'components/FormBtn';
 import error from 'assets/imgs/error404.png';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Error404(): JSX.Element {
+
+  const navigate = useNavigate();
+
   return (
     <div className='flex min-h-screen p-4 sm:px-8 pt-8 lg:px-36 justify-between lg:items-center bg-white'>
         <div className='relative' style={{zIndex: 2}}>
@@ -13,7 +16,7 @@ function Error404(): JSX.Element {
             <div className=' list-item ml-6 pt-1 text-lg'>Check if the link is correct</div>
             <div className=' list-item ml-6 pt-1 text-lg'>Check the page you are looking for has been deleted</div>
             <div style={{width: '250px'}}>
-                <FormBtn name='Back to homepage' fc={() => window.location.href = '/'} />
+                <FormBtn name='Back to homepage' fc={() => navigate('/')} />
             </div>
         </div>
         <div className='relative opacity-50 lg:opacity-100'>
